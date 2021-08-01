a426c6d050036881cf32da07194f72b2c6a67eab
Amir Iranmanesh <iranmanesh.ah@gmail.com>
2021-08-01 15:37:51 +0430

    add: user Gorm model

---
+++ db/sql/user.go
@@ -0,0 +1,33 @@
+package sql
+
+import (
+	"errors"
+	"gorm.io/gorm"
+)
+
+var createUserFailedError = errors.New("create user failed")
+var userNotFoundError = errors.New("user not found")
+
+type User struct {
+	gorm.Model
+	Name      string `gorm:"type:varchar(100); not null;" json:"name"`
+	Gender    string `gorm:"type:varchar(20);not null;" json:"gender"`
+	BirthYear int    `gorm:"not null;" json:"birth_year"`
+	Avatar    string `gorm:"type:varchar(200);" json:"avatar"`
+}
+
+func (u *User) Save(db *gorm.DB) error {
+
+	if result := db.Create(&u); result.Error != nil {
+		return createUserFailedError
+	}
+
+	return nil
+}
+
+func (u *User) Find(db *gorm.DB) error {
+	if result := db.First(&u, "id = ?", u.ID); result.Error != nil {
+		return userNotFoundError
+	}
+	return nil
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/amiranmanesh/imenaria-interview-task
 
 go 1.16
+
+require gorm.io/gorm v1.21.12
